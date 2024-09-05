f04257291515401be541913ddd7437a6c9408ab8
TanyaPeshkova <peshkova-2002.01@mail.ru>
2024-09-05 14:21:44 +0700

    скрипт калькулятора

---
+++ calc.js
@@ -0,0 +1,19 @@
+function clearDisplay()
+{
+	const display = document.getElementById('input');
+	display.value = '';
+}
+
+function append(value) {
+	const display = document.getElementById('input');
+	display.value += value
+}
+
+function calculate() {
+	const display = document.getElementById('input');
+	try {
+		display.value = eval(display.value);
+	} catch (error) {
+		display.value = 'Ошибка';
+	}
+}
